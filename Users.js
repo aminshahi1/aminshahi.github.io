606a7b97a9be4a945fff22e1ee7fc7a7ac3d6c5e
aminshahi1 <149341730+aminshahi1@users.noreply.github.com>
2023-10-30 02:06:30 +0100

    Add files via upload

---
+++ Users.js
@@ -0,0 +1,18 @@
+module.exports = [
+    {
+        id : 1,
+        name : "Hossein",
+        email : "amin@gmail.com"
+    },
+    {
+        id : 2,
+        name : "Ahmad",
+        email : "ahmad@gmail.com"
+    },
+    {
+        id : 3,
+        name : "Sadeq",
+        email : "sadeq@gmail.com"
+    },
+
+]
