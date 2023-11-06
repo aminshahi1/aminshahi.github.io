9c761f458ad73ff77b7f208b493ee7d7fd98fe90
aminshahi1 <it.aminshahi@gmail.com>
2023-11-06 01:10:14 +0100

    Updated

--- amin/hello.js
+++ amin/hello.js
@@ -5,28 +5,9 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({extended : false}));
 const mongoose = require('mongoose');
 
-const uri = "mongodb+srv://AminShahi:em77wJNhpFNXnSeQ@amin.vu1ezmz.mongodb.net/?retryWrites=true&w=majority";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+mongoose.connect("mongodb+srv://AminShahi:em77wJNhpFNXnSeQ@amin.vu1ezmz.mongodb.net/?retryWrites=true&w=majority").then(()=>{
+    console.log("Connected to Mongodb");
 });
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
 
 
 app.set('view engine', 'ejs');
