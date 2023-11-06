00289348c7167e7502a2a1765825808bc9aaa7a3
aminshahi1 <it.aminshahi@gmail.com>
2023-11-06 02:58:28 +0100

    updated

--- amin/hello.js
+++ amin/hello.js
@@ -10,31 +10,9 @@ const PORT = process.env.PORT || 8080;
 
 mongoose.set('strictQuery', false);
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = process.env.MONGO_URL;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
+mongoose.connect('mongodb://127.0.0.1:27017/test').then(()=>{
+    console.log("Connected to Mongodb");
 });
-
-async function run() {
-    try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
-    }
-}
-run().catch(console.dir);
 
 
 
